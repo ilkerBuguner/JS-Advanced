4c26676b779e1d2366965fed456dbe145a26b138
İlker Bugüner <ilker.buguner@gmail.com>
2020-06-06 22:23:35 +0300

    Solved some homework problems from DOM Manipulations lecture.

--- 05-DOM Manipulations/Homework/05. Encode-and-Decode-Messages/app.js
+++ 05-DOM Manipulations/Homework/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,31 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    document.querySelectorAll('button')[0].addEventListener('click', encodeAndSend);
+    document.querySelectorAll('button')[1].addEventListener('click', decodeAndRead);
+    const receiverTextArea = document.querySelectorAll('textArea')[1];
+
+    function encodeAndSend(e) {
+        const textArea = e.target.previousSibling.previousSibling;
+        const textAreaValue = textArea.value;
+        if (textAreaValue.length > 0) {
+            let encodedMessage = '';
+            for (let i = 0; i < textAreaValue.length; i++) {
+                const currentCharPrevAsciiValue = String.fromCharCode(textAreaValue[i].charCodeAt() + 1);
+                encodedMessage += currentCharPrevAsciiValue;
+            }
+            textArea.value = '';
+            receiverTextArea.value = encodedMessage;
+        }
+    }
+
+    function decodeAndRead(e) {
+        const receiverTextAreaValue = receiverTextArea.value;
+        if (receiverTextAreaValue.length > 0) {
+            let decodedMessage = '';
+            for (let i = 0; i < receiverTextAreaValue.length; i++) {
+                const currentCharNextAsciiValue = String.fromCharCode(receiverTextAreaValue[i].charCodeAt() - 1);
+                decodedMessage += currentCharNextAsciiValue;
+            }
+            receiverTextArea.value = decodedMessage;
+        }
+    }
 }
--- 05-DOM Manipulations/Homework/06. Table-Search-Engine/solution.js
+++ 05-DOM Manipulations/Homework/06. Table-Search-Engine/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-   //TODO...
+   document.getElementById('searchBtn').addEventListener('click', findMatches);
+
+   function findMatches(e) {
+      const inputToSearch = document.getElementById('searchField').value;
+
+      const names = Array.from(document.querySelectorAll('td'));
+      names.shift();
+      
+      for (const name of names) {
+         name.parentNode.classList.remove('select');
+      };
+      
+      for (const name of names) {
+         if(name.textContent.includes(inputToSearch)) {
+            name.parentNode.classList.add('select');
+         }
+      }
+   }
 }
