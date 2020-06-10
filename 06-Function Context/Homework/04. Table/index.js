ebefbf84f058cd45e954642cf747b297f44fbf57
İlker Bugüner <ilker.buguner@gmail.com>
2020-06-10 15:00:06 +0300

    Solved all homework problems from Function Context lecture.

--- 06-Function Context/Homework/04. Table/index.js
+++ 06-Function Context/Homework/04. Table/index.js
@@ -1,3 +1,15 @@
 function solve(){
-   // TODO
+   document.querySelector('tbody').addEventListener('click', changeBackground);
+
+   function changeBackground(e) {
+      if(e.target.nodeName === 'TD') {
+         if(e.target.parentNode.style['background-color']) {
+            e.target.parentNode.removeAttribute('style');
+         }
+         else {
+            Array.from(this.children).forEach(td => td.removeAttribute('style'));
+            e.target.parentNode.style['background-color'] = "#413f5e";
+         }
+      }
+   }
 }
